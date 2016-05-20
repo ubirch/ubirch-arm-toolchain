9a91a19e61d142e05156f1a2109c2e4a2c18c0b1
Matthias L. Jugel <leo@thinkberg.com>
2016-05-20 22:14:51 +0200

    make MBED_FLASH_DIR optional

--- cmake/Utils/flashing.cmake
+++ cmake/Utils/flashing.cmake
@@ -17,33 +17,35 @@ if (CMAKE_HOST_SYSTEM_NAME STREQUAL "Darwin")
   set(MBED_FLASH_DIR /Volumes/MBED)
 else ()
   if (NOT MBED_FLASH_DIR)
-    message(FATAL_ERROR "Please set MBED_FLASH_DIR to the directory where MBED mounts!")
+    message(WARNING "Please set MBED_FLASH_DIR to the directory where MBED mounts!")
   endif ()
 endif ()
 message(STATUS "MBED flash directory: ${MBED_FLASH_DIR}")
 
 macro(prepare_flash NAME)
-  if (ARM_NONE_EABI_SIZE)
-    add_custom_command(
-      TARGET ${NAME} POST_BUILD
-      COMMAND ${ARM_NONE_EABI_SIZE} $<TARGET_FILE_DIR:${NAME}>/${NAME}.elf
-    )
-  endif ()
+  if (MBED_FLASH_DIR)
+    if (ARM_NONE_EABI_SIZE)
+      add_custom_command(
+        TARGET ${NAME} POST_BUILD
+        COMMAND ${ARM_NONE_EABI_SIZE} $<TARGET_FILE_DIR:${NAME}>/${NAME}.elf
+      )
+    endif ()
 
-  if (BOARD MATCHES "ubirch.*")
-    # create special target that directly flashes
-    add_custom_target(${NAME}-flash
-      DEPENDS ${NAME}
-      COMMAND ${CMAKE_GDB} -x ${GDBINIT}_flash --batch $<TARGET_FILE_DIR:${NAME}>/${NAME}.elf
-      )
-  else ()
-    add_custom_command(TARGET ${NAME} POST_BUILD
-      COMMAND ${CMAKE_OBJCOPY} -Obinary $<TARGET_FILE:${NAME}> $<TARGET_FILE_DIR:${NAME}>/${NAME}.bin
-      )
-    add_custom_target(${NAME}-flash
-      DEPENDS ${NAME}
-      COMMAND ${CMAKE_COMMAND} -E copy $<TARGET_FILE_DIR:${NAME}>/${NAME}.bin ${MBED_FLASH_DIR}
-      )
+    if (BOARD MATCHES "ubirch.*")
+      # create special target that directly flashes
+      add_custom_target(${NAME}-flash
+        DEPENDS ${NAME}
+        COMMAND ${CMAKE_GDB} -x ${GDBINIT}_flash --batch $<TARGET_FILE_DIR:${NAME}>/${NAME}.elf
+        )
+    else ()
+      add_custom_command(TARGET ${NAME} POST_BUILD
+        COMMAND ${CMAKE_OBJCOPY} -Obinary $<TARGET_FILE:${NAME}> $<TARGET_FILE_DIR:${NAME}>/${NAME}.bin
+        )
+      add_custom_target(${NAME}-flash
+        DEPENDS ${NAME}
+        COMMAND ${CMAKE_COMMAND} -E copy $<TARGET_FILE_DIR:${NAME}>/${NAME}.bin ${MBED_FLASH_DIR}
+        )
+    endif ()
   endif ()
   # print out some helpful hints to debug
   add_custom_target(${NAME}-gdb
